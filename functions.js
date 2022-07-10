865227ad99a459557e7684209738e115a7b0eb94
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-07-10 10:37:50 +0430

    Added new test

--- functions.js
+++ functions.js
@@ -1,5 +1,14 @@
 const functions = {
   add: (num1, num2) => num1 + num2,
+  isNull: () => null,
+  checkValue: (x) => x,
+  createUser: () => {
+    const user = {
+      firstName: "Mohammadhossein",
+      lastName: "sd",
+    };
+    return user;
+  },
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -1,9 +1,36 @@
 const functions = require("./functions");
 
-test("Adds 2 + 2 equal 4", () => {
+// toBe
+test("Adds 2 + 2 to equal 4", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
 
+// NOT toBe
 test("Adds 2 + 2 to NOT equal 5", () => {
   expect(functions.add(2, 2)).not.toBe(5);
+});
+
+// check for truthy & falsy values
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the oppsite of toBeUndefined
+// toBeTrithy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+// toBeNull
+test("Should be null", () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+// toBeFalsy
+test("Should be falsy", () => {
+  expect(functions.checkValue(null)).toBeFalsy();
+});
+
+// toEqual
+test("User should be Mohammadhossein object", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Mohammadhossein",
+    lastName: "sd",
+  });
 });
